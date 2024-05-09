d241f3825d98e5e70cabef58a03a3cf00fa30bcd
fumipi <toyodafumiko@gmail.com>
2024-05-09 16:48:12 +0900

    Changed wording to be more generic

--- app.py
+++ app.py
@@ -44,18 +44,18 @@ def store_svg_to_github(svg_content, commit_message):
     return response
 
 def main():
-    st.title('SVG Uploader')
+    st.title('File Uploader')
     st.text('自走式ペンプロッタープリンターが描く画像をアップロードするアプリです。 \n画像がアップロードできたらロボットを動かすシミュレーションを開始します。')
     
     # ファイルアップロードUI
-    uploaded_file = st.file_uploader('SVG画像を選択', type=['svg'])
+    uploaded_file = st.file_uploader('画像を選択', type=['svg'])
     
     if uploaded_file is not None:
         # アップロードされたファイルを読み込む
         svg_content = uploaded_file.read()
         
         # SVGファイルをGithubに登録
-        if st.button('SVGファイルを登録してシミュレーションをスタート'):
+        if st.button('画像ファイルを登録してシミュレーションをスタート'):
             commit_message = 'Add or update SVG file'  # デフォルトのコミットメッセージを設定
             response = store_svg_to_github(svg_content, commit_message)
             
@@ -64,7 +64,7 @@ def main():
                 # webots.cloudのシミュレーションを開くURL
                 simulation_url = "https://webots.cloud/run?version=R2023b&url=https%3A%2F%2Fgithub.com%2Ffumipi%2Fautonomous_pen_plotter_concept%2Fblob%2Fmain%2Fworlds%2Fpenbot.wbt&type=demo"
                 
-                st.markdown(f'SVGファイルが登録できました。[ここ]({simulation_url})をクリックしてシミュレーションを開いてください。', unsafe_allow_html=True)
+                st.markdown(f'画像ファイルが登録できました。[ここ]({simulation_url})をクリックしてシミュレーションを開いてください。', unsafe_allow_html=True)
             else:
                 st.error(f'Failed to store the SVG file. Status code: {response.status_code}')
                 st.error(f'Error message: {response.text}')
